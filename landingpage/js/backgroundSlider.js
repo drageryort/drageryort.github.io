3ba0301eecfbc59ca1cec501e63344ffab7f4761
drageryort <drageryort@gmail.com>
2018-05-09 16:16:12 +0300

    add slider, new background and hover style for header and footer

---
+++ landingpage/js/backgroundSlider.js
@@ -0,0 +1,5 @@
+    $('.header').fullClip({
+      images: ['images/headerForestBackground.png','images/headerSeaBackground.png','images/headerCityBackground.png','images/headerMountainBackground.png'],
+      transitionTime: 2000,
+      wait: 3000
+    });
