b2f0e0fbdd262708775b0cedfe2925c510cb1787
drageryort <drageryort@gmail.com>
2018-06-17 17:31:44 +0300

    add new project touragency

---
+++ touragency/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require ('gulp'),
+	concatCss = require('gulp-concat-css'),
+	cleanCSS = require('gulp-clean-css'),
+	uncss = require('gulp-uncss'),
+	concat = require('gulp-concat'),
+	uglyfly = require('gulp-uglyfly'),
+ 	imagemin = require('gulp-imagemin');
+
+
+
+
+
+
+
+gulp.task ('css', function (){
+	return gulp.src('app/css/*.css')
+    .pipe(concatCss('stylesheet-min.css'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css/'));
+})
+
+
+gulp.task('scripts', function() {
+  return gulp.src('app/js/*.js')
+    .pipe(concat('scripts-min.js'))
+    .pipe(uglyfly())
+    .pipe(gulp.dest('dist/js/'));
+});
+
+
+gulp.task('image', () =>
+    gulp.src('app/image/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/image'))
+);
